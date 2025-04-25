160e3c46ffb989e2fb21a78c6f19858532803616
Hugo Uparela <hugouparela21@gmail.com>
2025-04-25 04:21:19 -0500

    Simplificar la función mostrar_catalogos al utilizar listar_catalogos_por_categoria para obtener categorías

--- ver_catalogos/mostrar_catalogos.py
+++ ver_catalogos/mostrar_catalogos.py
@@ -1,17 +1,6 @@
-import os
+from .utilidades import listar_catalogos_por_categoria
 from .interfaz import crear_ventana_catalogos
-from .utilidades import obtener_categorias, obtener_archivos_json
 
 def mostrar_catalogos():
-    base_path = "catalogos"
-    categorias = obtener_categorias()
-    categorias_con_archivos = {}
-
-    for categoria in categorias:
-        categoria_path = os.path.join(base_path, categoria)
-        archivos = obtener_archivos_json(categoria_path)
-        if archivos:
-            categorias_con_archivos[categoria] = (categoria_path, archivos)
-
-    if categorias_con_archivos:
-        crear_ventana_catalogos(categorias_con_archivos)
+    cats = listar_catalogos_por_categoria()
+    crear_ventana_catalogos(cats)
